dd7492afd2c2f8258e092b04ef3f51e29937b103
Sathya Raghunathan <satya_raghunathan@yahoo.com>
2015-08-20 11:36:06 +0000

    4th version

--- helloworld.c
+++ helloworld.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
 #include "module1.h"
+#include "module2.h"
 int main()
 {
 	printf("Third version of Hello World\n");
 	module1fun();
+	module2fun();
 	return 0;
 
 }
---
+++ module2.h
@@ -0,0 +1,4 @@
+int module2fun(){
+	printf("Hi there. I went to the next level\n");
+	printf("Yahoo!!!");
+}
